6c585eac744d14e5eb9ad05772e7e105be664901
jayfarand <owlasylum@gmail.com>
2017-01-04 07:49:35 -0500

    Updating For Future Changes

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,5 +2,9 @@
  * Created by OWL on 1/3/2017.
  */
  import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
